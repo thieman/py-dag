4bc45406a301fa8c5a869e0c01c1ba7b38c57160
Travis Thieman <travis.thieman@gmail.com>
2014-11-13 12:00:43 -0500

    Initial release

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='py-dag',
+      version='0.0.1',
+      description='Directed acyclic graph implementation',
+      url='https://github.com/thieman/py-dag',
+      author='Travis Thieman',
+      author_email='travis.thieman@gmail.com',
+      license='MIT',
+      packages=['dag'],
+      zip_safe=False)
