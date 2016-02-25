465ceb648a4b778bbfd03722d57b90793e3abce8
Travis Thieman <travis.thieman@gmail.com>
2016-02-25 14:00:43 -0500

    Add idempotent add and delete, bump to 2.4.0

--- dag/__init__.py
+++ dag/__init__.py
@@ -19,6 +19,11 @@ class DAG(object):
             raise KeyError('node %s already exists' % node_name)
         graph[node_name] = set()
 
+    def add_node_if_not_exists(self, node_name, graph=None):
+        try:
+            self.add_node(node_name, graph=graph)
+        except KeyError:
+            pass
 
     def delete_node(self, node_name, graph=None):
         """ Deletes this node and all edges referencing it. """
@@ -32,6 +37,11 @@ class DAG(object):
             if node_name in edges:
                 edges.remove(node_name)
 
+    def delete_node_if_exists(self, node_name, graph=None):
+        try:
+            self.delete_node(node_name, graph=graph)
+        except KeyError:
+            pass
 
     def add_edge(self, ind_node, dep_node, graph=None):
         """ Add an edge (dependency) between the specified nodes. """
